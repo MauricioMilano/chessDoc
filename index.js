61f2bd508f7d57c4af5ea417e5e44afa90b0afd8
Mauricio Milano <mauricio.milano@.ibm.com>
2021-03-05 17:03:22 -0300

    teste 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 0000
+const port = 3000
 app.use('/', express.static(__dirname + '/dist'));
 
 app.use('*',express.static(__dirname + '/dist/404.html'))
