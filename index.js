b9c55cadba8554c59387217e7add056ea5da07f3
Mauricio Milano <mauricio.milano@.ibm.com>
2021-04-22 21:37:55 -0300

    Regras

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
-app.use('/', express.static(__dirname + '/dist'));
-
-app.use('*',express.static(__dirname + '/dist/404.html'))
+app.use('/', express.static(__dirname + '/'));
+app.use('*',express.static(__dirname + '/404.html'))
 app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
